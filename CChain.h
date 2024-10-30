576046ec1a6621b81e057c283590603ad0306c94
Ayesha Siddiqui <155626270+Ayesha-Siddiqui1234@users.noreply.github.com>
2024-10-30 15:19:28 +0500

    Update CChain.h by ayesha siddiqui

--- CChain.h
+++ CChain.h
@@ -1,23 +1,56 @@
+// #ifndef CCHAIN_H
+// #define CCHAIN_H
+// #include "CBlock.h"
+// #include <vector>
+
+// namespace blockchain
+// {
+//     class CChain
+//     {
+//     private:
+//         std::vector<CBlock> mChain;
+//         CBlock* mCurrentBlock;
+//         int mDifficulty;
+//     public:
+//         CChain(int difficulty);
+//         void appendToCurrentBlock(uint8_t* data, uint32_t size);
+//         void nextBlock();
+//         CBlock* getCurrentBlock();
+
+//     };
+// }
+
+// #endif
+
 #ifndef CCHAIN_H
 #define CCHAIN_H
+
 #include "CBlock.h"
-#include <vector>
 
 namespace blockchain
 {
     class CChain
     {
     private:
-        std::vector<CBlock> mChain;
-        CBlock* mCurrentBlock;
+        struct Node
+        {
+            CBlock block;
+            Node* next;
+            Node(CBlock blk) : block(blk), next(nullptr) {}
+        };
+
+        Node* mHead;      // Pointer to the first block in the chain
+        Node* mTail;      // Pointer to the last block in the chain
         int mDifficulty;
+
     public:
         CChain(int difficulty);
+        ~CChain(); // Destructor to free allocated memory
+
         void appendToCurrentBlock(uint8_t* data, uint32_t size);
         void nextBlock();
         CBlock* getCurrentBlock();
-
     };
 }
 
-#endif
+#endif // CCHAIN_H
